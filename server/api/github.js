f9794977a8214637896fe6135125d285ca7ffa91
Stanislav Levitt <stanislavlevitt@yahoo.com>
2022-09-25 12:24:02 -0400

    combine An option to be able to view the top 10, 50, 100 repositories and date sorting

--- server/api/github.js
+++ server/api/github.js
@@ -19,3 +19,27 @@ router.get("/", async(req,res)=>{
   if(data.incomplete_results) res.send({Warning: "Results may be inaccurate due to high search results", repositories: list})
   else res.send({repositories: list})
 })
+
+router.get('/:limit', async (req,res)=>{
+  let list = []
+  let response = await fetch(generateURL(req.params.limit, req.query.date))
+  let data = await response.json()
+  data.items.forEach(repo =>{
+    list.push({
+      starCount: repo.stargazers_count,
+      repoName: repo.name,
+      date: repo.created_at,
+      repoUrl: repo.html_url,
+    })
+  })
+
+  if(data.incomplete_results) res.send({Warning: "Results may be inaccurate due to high search results", repositories: list})
+  else res.send({repositories: list})
+})
+
+const generateURL = (limit, date) =>{
+  let url = BASE_URL.concat(`&per_page=${limit}`)
+  if(date) url = url.concat(`&q=created:>${date}`)
+
+  return url
+}
