edd3b3fc4e1abc450fc1b85203ff63cf1a448b16
Stanislav Levitt <stanislavlevitt@yahoo.com>
2022-09-25 12:30:59 -0400

    modularize response code

--- server/api/github.js
+++ server/api/github.js
@@ -4,37 +4,14 @@ module.exports = router
 const BASE_URL = "https://api.github.com/search/repositories?q=stars:%3E=1&sort=stars&order=desc"
 
 router.get("/", async(req,res)=>{
-  let list = []
   let response = await fetch(BASE_URL)
-  let data = await response.json()
-  data.items.forEach(repo =>{
-    list.push({
-      starCount: repo.stargazers_count,
-      repoName: repo.name,
-      date: repo.created_at,
-      repoUrl: repo.html_url,
-    })
-  })
-
-  if(data.incomplete_results) res.send({Warning: "Results may be inaccurate due to high search results", repositories: list})
-  else res.send({repositories: list})
+  displayResponse(response, res)
 })
 
 router.get('/:limit', async (req,res)=>{
-  let list = []
   let response = await fetch(generateURL(req.params.limit, req.query.date))
-  let data = await response.json()
-  data.items.forEach(repo =>{
-    list.push({
-      starCount: repo.stargazers_count,
-      repoName: repo.name,
-      date: repo.created_at,
-      repoUrl: repo.html_url,
-    })
-  })
+  displayResponse(response, res)
 
-  if(data.incomplete_results) res.send({Warning: "Results may be inaccurate due to high search results", repositories: list})
-  else res.send({repositories: list})
 })
 
 const generateURL = (limit, date) =>{
@@ -42,4 +19,20 @@ const generateURL = (limit, date) =>{
   if(date) url = url.concat(`&q=created:>${date}`)
 
   return url
+}
+
+const displayResponse = async (response, res) =>{
+  let list = []
+  let data = await response.json()
+  data.items.forEach(repo =>{
+    list.push({
+      starCount: repo.stargazers_count,
+      repoName: repo.name,
+      date: repo.created_at,
+      repoUrl: repo.html_url,
+    })
+  })
+
+  if(data.incomplete_results) res.send({Warning: "Results may be inaccurate due to high search results", repositories: list})
+  else res.send({repositories: list})
 }
