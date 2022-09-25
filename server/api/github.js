4ee9026ef3710d9b4a0a6d995923a493ca0f653e
Stanislav Levitt <stanislavlevitt@yahoo.com>
2022-09-25 12:16:58 -0400

    get the api call for A list of the most popular repositories, sorted by number of stars working

--- server/api/github.js
+++ server/api/github.js
@@ -1,6 +1,21 @@
 const router = require('express').Router()
 module.exports = router
 
+const BASE_URL = "https://api.github.com/search/repositories?q=stars:%3E=1&sort=stars&order=desc"
+
 router.get("/", async(req,res)=>{
-  res.send('Welcome to Github NodeJS API!')
+  let list = []
+  let response = await fetch(BASE_URL)
+  let data = await response.json()
+  data.items.forEach(repo =>{
+    list.push({
+      starCount: repo.stargazers_count,
+      repoName: repo.name,
+      date: repo.created_at,
+      repoUrl: repo.html_url,
+    })
+  })
+
+  if(data.incomplete_results) res.send({Warning: "Results may be inaccurate due to high search results", repositories: list})
+  else res.send({repositories: list})
 })
